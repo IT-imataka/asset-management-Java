808d5a83dc774c65d581cda1c7791ab3f09a809c
IT-imataka <takashi6128_gakky@outlook.com>
2026-02-17 23:52:27 +0900

    Initial Commit : Spring Boot Hello World

---
+++ asset-management/src/main/java/com/example/asset_management/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.asset_management;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController{
+    @GetMapping("/")
+    public String hello(){
+        return "Hello World! Java Enviroment is Ready.";
+    }
+}
