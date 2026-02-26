e4744943e6dccdfc27c66f3d67029ac9cb743129
IT-imataka <takashi6128_gakky@outlook.com>
2026-02-26 22:52:22 +0900

    add: comment

--- asset-management/src/main/java/com/example/asset_management/service/AssetService.java
+++ asset-management/src/main/java/com/example/asset_management/service/AssetService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 @Service
 public class AssetService {
+    //　アノテーションのすぐ下に実行したいメソッドを書く
     public List<Asset> getAssets(){
         return null;
     }
@@ -14,3 +15,4 @@ public class AssetService {
 
     }
 }
+
