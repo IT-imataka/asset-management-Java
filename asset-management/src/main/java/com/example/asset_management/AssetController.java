847b92673c0f9815e9915ba2cf35d6e3dc47a562
IT-imataka <takashi6128_gakky@outlook.com>
2026-02-27 18:40:35 +0900

    refactor: divide service

--- asset-management/src/main/java/com/example/asset_management/AssetController.java
+++ asset-management/src/main/java/com/example/asset_management/AssetController.java
@@ -1,47 +1,28 @@
 package com.example.asset_management;
 import com.example.asset_management.entity.Asset;
-import com.example.asset_management.enums.Category;
-import com.example.asset_management.enums.Status;
+import com.example.asset_management.service.AssetService;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @CrossOrigin (origins = "http://localhost:5173")
 public class AssetController {
-    // 使い捨てのコントローラーから保存できるようにする
-    // これがフィールド
-    private final List<Asset> assetList = new ArrayList<>();
 
-    // 初回起動時にだけ実行するコンストラクタ
-    public AssetController(){
-        // １件目
-        Asset mac = new Asset();
-        mac.setId("A0001");
-        mac.setName("MacBook Air");
-        mac.setCategory(Category.PC);
-        mac.setStatus(Status.ASSIGNED);
-        mac.setUserName("我");
-        assetList.add(mac);
+    // フィールド
+    private final AssetService assetService;
 
-        // モニター
-        Asset monitor = new Asset();
-        monitor.setId("A0002");
-        monitor.setName("外部モニター");
-        monitor.setCategory(Category.MONITOR);
-        monitor.setStatus(Status.ASSIGNED);
-        monitor.setUserName(null);
-        assetList.add(monitor);
+    // コンストラクタ
+    public AssetController(AssetService assetService){
+        this.assetService = assetService;
     }
-
     @GetMapping("/asset")
     public List<Asset> getAssets(){
-        return assetList;
+        return assetService.getAssets();
     }
     @PostMapping("/asset")
     public void postAssets(@RequestBody Asset asset){
-        assetList.add(asset);
+        assetService.addAsset(asset);
         System.out.println("登録しました:" + asset.getName());
     }
 }
--- asset-management/src/main/java/com/example/asset_management/service/AssetService.java
+++ asset-management/src/main/java/com/example/asset_management/service/AssetService.java
@@ -1,18 +1,45 @@
 package com.example.asset_management.service;
 
 import com.example.asset_management.entity.Asset;
+import com.example.asset_management.enums.Category;
+import com.example.asset_management.enums.Status;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+//　アノテーションのすぐ下に実行したいメソッドを書くのはControllerのみ
 @Service
 public class AssetService {
-    //　アノテーションのすぐ下に実行したいメソッドを書く
+
+    // これがフィールド
+    private final List<Asset> assetList = new ArrayList<>();
+
+    // 初回起動時にだけ実行するコンストラクタ
+    public AssetService(){
+        // １件目
+        Asset mac = new Asset();
+        mac.setId("A0001");
+        mac.setName("MacBook Air");
+        mac.setCategory(Category.PC);
+        mac.setStatus(Status.ASSIGNED);
+        mac.setUserName("我");
+        assetList.add(mac);
+
+        // モニター
+        Asset monitor = new Asset();
+        monitor.setId("A0002");
+        monitor.setName("外部モニター");
+        monitor.setCategory(Category.MONITOR);
+        monitor.setStatus(Status.ASSIGNED);
+        monitor.setUserName(null);
+        assetList.add(monitor);
+    }
     public List<Asset> getAssets(){
-        return null;
+        return assetList;
     }
     public void addAsset(Asset asset){
-
+        assetList.add(asset);
     }
 }
 
