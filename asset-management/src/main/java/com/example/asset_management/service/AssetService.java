6ea2f8464f91b9585db5e111870e09e590000335
IT-imataka <takashi6128_gakky@outlook.com>
2026-02-28 23:12:17 +0900

    feat: refactor Service layerd with implement Repo and DB

--- asset-management/src/main/java/com/example/asset_management/service/AssetService.java
+++ asset-management/src/main/java/com/example/asset_management/service/AssetService.java
@@ -4,6 +4,7 @@ import com.example.asset_management.entity.Asset;
 import com.example.asset_management.enums.Category;
 import com.example.asset_management.enums.Status;
 import org.springframework.stereotype.Service;
+import com.example.asset_management.repository.AssetRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,12 +12,12 @@ import java.util.List;
 //　アノテーションのすぐ下に実行したいメソッドを書くのはControllerのみ
 @Service
 public class AssetService {
-
     // これがフィールド
-    private final List<Asset> assetList = new ArrayList<>();
+    private final AssetRepository assetRepository;
 
     // 初回起動時にだけ実行するコンストラクタ
-    public AssetService(){
+    public AssetService(AssetRepository assetRepository){
+        this.assetRepository = assetRepository;
         // １件目
         Asset mac = new Asset();
         mac.setId("A0001");
@@ -24,7 +25,8 @@ public class AssetService {
         mac.setCategory(Category.PC);
         mac.setStatus(Status.ASSIGNED);
         mac.setUserName("我");
-        assetList.add(mac);
+        // assetRepository.add(mac);
+        assetRepository.save(mac);
 
         // モニター
         Asset monitor = new Asset();
@@ -33,13 +35,15 @@ public class AssetService {
         monitor.setCategory(Category.MONITOR);
         monitor.setStatus(Status.ASSIGNED);
         monitor.setUserName(null);
-        assetList.add(monitor);
+        // assetRepository.add(monitor);
+        assetRepository.save(monitor);
     }
     public List<Asset> getAssets(){
-        return assetList;
+        return assetRepository.findAll();
     }
     public void addAsset(Asset asset){
-        assetList.add(asset);
+        // assetList.add(asset);
+        assetRepository.save(asset);
     }
 }
 
